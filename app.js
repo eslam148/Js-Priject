6231232ae407a6726fb100cb63fd8fa8bed2ee7e
eslam148 <islam.18333388@compit.aun.edu.eg>
2024-12-13 23:43:32 +0200

    add footer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { routes } from "./router.js";
 
 const app = document.getElementById("app");
+const scriptcontetn = document.createElement("scriptConetent");
 let currentStylesheet = null;
 let currentScript = null;
 
@@ -32,8 +33,8 @@ function loadScript(scriptPath) {
     const script = document.createElement("script");
     script.remove()
     script.src = scriptPath;
-    document.head.appendChild(script);
-    currentScript = script;
+    scriptcontetn.append(script);
+     currentScript = script;
 }
 async function handleRoute() {
      const hash = window.location.hash.substring(1) || "home";
@@ -58,10 +59,21 @@ window.addEventListener("load", handleRoute);
 async function loadNavbar() {
     const navbarHTML = await loadComponent("Components/navbarComponent/navbar.html");
     document.body.insertAdjacentHTML("afterbegin", navbarHTML);
-    const navbarCSS = document.createElement("link");
-    navbarCSS.rel = "stylesheet";
-    navbarCSS.href = "Components/navbarComponent/navbar.css";
-    document.head.appendChild(navbarCSS);
+    loadStylesheet("Components/navbarComponent/navbar.css")
+    loadScript("Components/navbarComponent/navbar.js")
+    // const navbarCSS = document.createElement("link");
+    // navbarCSS.rel = "stylesheet";
+    // navbarCSS.href = "Components/navbarComponent/navbar.css";
+    // document.head.appendChild(navbarCSS);
 }
-
+async function loadFooter() {
+    const footerHTML = await loadComponent("Components/footerComponent/footer.html");
+    app.insertAdjacentHTML("afterend", footerHTML);
+    const footerCSS = document.createElement("link");
+    loadStylesheet("Components/footerComponent/footer.css")
+    loadScript("Components/footerComponent/footer.js")
+    
+}
 window.addEventListener("load", loadNavbar);
+window.addEventListener("load", loadFooter);
+
