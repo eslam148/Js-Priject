95f249fa6471ef97dc4e1aa851a1d55243d893d3
davzekry <37640599+davzekry@users.noreply.github.com>
2024-12-13 15:33:17 +0200

    Adding images and tailwind link

--- router.js
+++ router.js
@@ -1,6 +1,5 @@
 export const routes = {
     home: { html: "Components/homeComponenet/home.html", css: "Components/homeComponenet/home.css" ,script:"Components/homeComponenet/home.js"},
     about: { html: "Components/aboutComponenet/about.html", css: "Components/aboutComponenet/about.css" ,script:"Components/aboutComponenet/about.js"},
-
   };
   
