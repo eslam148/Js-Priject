08409f00fc6be2eafb4549aad248aded0ee1f942
eslam148 <islam.18333388@compit.aun.edu.eg>
2024-12-15 01:00:30 +0200

    add product details and login page

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
 export const routes = {
     home: { html: "Components/homeComponenet/home.html", css: "Components/homeComponenet/home.css" ,script:"Components/homeComponenet/home.js"},
     Cart: { html: "Components/CartComponent/Cart.html", css: "Components/CartComponent/Cart.css" ,script:"Components/CartComponent/Cart.js"},
+    logIn: { html: "Components/logInComponent/login.html", css: "Components/logInComponent/login.css" ,script:"Components/logInComponent/login.js"},
+    product: { html: "Components/ProductComponent/product.html", css: "Components/ProductComponent/product.css" ,script:"Components/ProductComponent/product.js"},
+
   };
   
