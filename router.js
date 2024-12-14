feeef0e5a6f95f5df011e1181ccee596f85cef41
eslam148 <islam.18333388@compit.aun.edu.eg>
2024-12-14 23:51:31 +0200

    add Cart

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 export const routes = {
     home: { html: "Components/homeComponenet/home.html", css: "Components/homeComponenet/home.css" ,script:"Components/homeComponenet/home.js"},
-    about: { html: "Components/aboutComponenet/about.html", css: "Components/aboutComponenet/about.css" ,script:"Components/aboutComponenet/about.js"},
+    Cart: { html: "Components/CartComponent/Cart.html", css: "Components/CartComponent/Cart.css" ,script:"Components/CartComponent/Cart.js"},
   };
   
