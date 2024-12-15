fc67b10244b428508b71263aad1b1649175337b0
eslam148 <islam.18333388@compit.aun.edu.eg>
2024-12-15 17:45:47 +0200

    navbar completed successfuly

--- Components/navbarComponent/navbar.js
+++ Components/navbarComponent/navbar.js
@@ -4,7 +4,7 @@
 var bg_nav = document.getElementById("nav");
 
       if (wind.scrollY > 0) {
-        bg_nav.style= "background-color: rgba(0, 0, 0, 0.88);hieght:11vh;transition:0.6s;padding-top:5px;";
+        bg_nav.style= "background-color: rgba(0, 0, 0, 0.88);height:10vh;transition:0.6s;padding-top:5px;";
       } else {
         bg_nav.style = "background-color:transparent;transition:0.6s;";
       }
