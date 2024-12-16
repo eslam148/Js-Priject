ae193bd939856ef81d5c129359e3e72f89244956
eslam148 <islam.18333388@compit.aun.edu.eg>
2024-12-16 22:37:51 +0200

    add card component

--- router.js
+++ router.js
@@ -3,6 +3,6 @@ export const routes = {
     Cart: { html: "Components/CartComponent/Cart.html", css: "Components/CartComponent/Cart.css" ,script:"Components/CartComponent/Cart.js"},
     logIn: { html: "Components/logInComponent/login.html", css: "Components/logInComponent/login.css" ,script:"Components/logInComponent/login.js"},
     product: { html: "Components/ProductComponent/product.html", css: "Components/ProductComponent/product.css" ,script:"Components/ProductComponent/product.js"},
-
+    card: { html: "Components/cardsComponenet/cards.html", css: "Components/cardsComponenet/cards.css" ,script:"Components/cardsComponenet/cards.js"},
   };
   
