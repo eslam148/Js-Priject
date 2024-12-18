a9fe272a3d4e56b7af7dc1512e31e49ab737af1e
eslam148 <islam.18333388@compit.aun.edu.eg>
2024-12-18 16:04:58 +0200

    add

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 export const routes = {
     home: { html: "Components/homeComponenet/home.html", css: "Components/homeComponenet/home.css" ,script:"Components/homeComponenet/home.js"},
-    Cart: { html: "Components/CartComponent/Cart.html", css: "Components/CartComponent/Cart.css" ,script:"Components/CartComponent/Cart.js"},
+    Cart: { html: "Components/CartComponent/cart.html", css: "Components/cartComponent/Cart.css" ,script:"Components/cartComponent/Cart.js"},
     logIn: { html: "Components/logInComponent/login.html", css: "Components/logInComponent/login.css" ,script:"Components/logInComponent/login.js"},
-    product: { html: "Components/ProductComponent/product.html", css: "Components/ProductComponent/product.css" ,script:"Components/ProductComponent/product.js"},
+    product: { html: "Components/productComponent/product.html", css: "Components/productComponent/product.css" ,script:"Components/productComponent/product.js"},
     card: { html: "Components/cardsComponenet/cards.html", css: "Components/cardsComponenet/cards.css" ,script:"Components/cardsComponenet/cards.js"},
   };
   
